abcc70a5363037069c64a0a67b66d86411228729
kosipov <mail@kosipov.ru>
2023-04-03 19:20:06 +0300

    first test

---
+++ tests/unit/Services/StudentServiceTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace unit\Services;
+
+use Kosipov\StudentTest\Service\StudentService;
+use PHPUnit\Framework\TestCase;
+
+class StudentServiceTest extends TestCase
+{
+
+    public function testDismissStudent()
+    {
+        //...
+        self::assertEquals(1,1);
+    }
+}
